395ebb12faad772d3f50341882ef8e1f2fe88c2e
Ben <ben@ryansmac.local>
2019-09-14 00:51:31 -0700

    Added super sleek animation

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var $root = $('html, body');
+
+  $('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+
+    return false;
+  });
+});
