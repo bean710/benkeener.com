7552dd7d7055ce21ff3c1e22a5850aef0f0ff056
Ben Keener <keener4christ@gmail.com>
2019-11-10 15:21:36 -0800

    Excludes jpeg images from sync

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 git checkout master
-aws s3 sync ./public/ s3://www.benkeener.com/
+aws s3 sync ./public/ s3://www.benkeener.com/ --exclude "*.jpeg"
 aws s3 sync s3://www.benkeener.com s3://benkeener.com/
-git checkout - 
+git checkout -
